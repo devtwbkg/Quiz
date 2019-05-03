ce32a0e38fd08e5749a10c037cb1539394f0a2fd
Wittaya <wittaya.b@lansingbs.com>
2019-05-03 15:33:06 +0700

    Initial Commit

---
+++ QuizeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizeGame
+//
+//  Created by TWBKG on 3/5/2562 BE.
+//  Copyright © 2562 TWBKG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
