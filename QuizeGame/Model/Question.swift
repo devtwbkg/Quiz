710d4e7ebcf9a215ec77fc877ae40b105b887d8b
Wittaya <wittaya.b@lansingbs.com>
2019-05-04 16:12:41 +0700

    init

---
+++ QuizeGame/Model/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  QuizeGame
+//
+//  Created by TWBKG on 3/5/2562 BE.
+//  Copyright © 2562 TWBKG. All rights reserved.
+//
+
+import Foundation
---
+++ QuizeGame/Model/QuestionBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionBank.swift
+//  QuizeGame
+//
+//  Created by TWBKG on 3/5/2562 BE.
+//  Copyright © 2562 TWBKG. All rights reserved.
+//
+
+import Foundation
